caa70b708b94775c9fbec613b03ebfd38f09a9d7
Brandyn Vay <brandynvay@protonmail.com>
2019-08-08 23:14:07 -0700

    done

---
+++ __tests__/app.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('File test', () => {
+  it('Does the file change work?', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 'use strict';
 
 const fs = require('fs');
-const events = require('./src/models/events-module.js');
 
+const events = require('./src/models/events-module.js');
 require('./src/models/logger-module.js');
 
-events.emit();
-
-const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
+/**
+ *
+ *
+ * @param {*} file
+ */
+const /**
+ *
+ *
+ * @param {*} err
+ * @param {*} data
+ */
+  alterFile = (file) => {
+    fs.readFile( file, (err, data) => {
+      if(err) {
+        events.emit('error', err);
+      }
+      let text = data.toString().toUpperCase();
+    
+      fs.writeFile( file, Buffer.from(text), (err, data) => {
+        if(err) { 
+          events.emit('error', err);
+        }
+        events.emit('success', `The ${file} file has been saved!`);
+      });
     });
-  });
-};
+  };
 
 let file = process.argv.slice(2).shift();
 alterFile(file);
